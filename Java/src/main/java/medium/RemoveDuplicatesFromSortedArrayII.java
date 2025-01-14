206e0b25ed3a8f071bbf8880c8060c795f5aa480
MarioLuiz <marioluiz.arruda@gmail.com>
2025-01-14 16:06:03 -0400

    feat: Remove Duplicates from Sorted Array II

--- Java/src/main/java/medium/RemoveDuplicatesFromSortedArrayII.java
+++ Java/src/main/java/medium/RemoveDuplicatesFromSortedArrayII.java
@@ -1,8 +1,9 @@
-package easy;
+package medium;
 
 import java.util.Arrays;
-// https://leetcode.com/problems/remove-duplicates-from-sorted-array/?envType=study-plan-v2&envId=top-interview-150
-public class RemoveDuplicatesFromSortedArray {
+
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/?envType=study-plan-v2&envId=top-interview-150
+public class RemoveDuplicatesFromSortedArrayII {
     public static void main(String[] args) {
         int[] nums = {0,0,1,1,1,2,2,3,3,4};
         int repetidos = removeDuplicates(nums);
@@ -13,10 +14,9 @@ public class RemoveDuplicatesFromSortedArray {
     public static int removeDuplicates(int[] nums) {
         if (nums.length == 0 ) return 0;
 
-        int j = 1;
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[j - 1]) {
+        int j = 2;
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[j - 2]) {
                 nums[j] = nums[i];
                 j++;
             }
