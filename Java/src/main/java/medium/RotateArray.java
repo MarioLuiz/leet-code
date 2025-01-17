4685fce57ed4a7d406e33e9696ba63ff83cb76d3
MarioLuiz <marioluiz.arruda@gmail.com>
2025-01-17 18:22:23 -0400

    feat: Rotate Array

--- Java/src/main/java/medium/RotateArray.java
+++ Java/src/main/java/medium/RotateArray.java
@@ -1,24 +1,31 @@
 package medium;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
-// https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/?envType=study-plan-v2&envId=top-interview-150
+//https://leetcode.com/problems/rotate-array/description/?envType=study-plan-v2&envId=top-interview-150
 public class RotateArray {
     public static void main(String[] args) {
-        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-        int k = 3;
+        int[] nums = {-1,-100,3,99};
+        int k = 2;
         int[] rodado = rotate(nums, k);
         System.out.println("Nums: " + Arrays.toString(rodado));
     }
 
-    public static int[] rotate(int[] nums, int l) {
+    public static int[] rotate(int[] nums, int k) {
+        int tamanhoArray = nums.length;
+        int[] rotated = new int[tamanhoArray];
+        k = k % tamanhoArray;
+        int[] inicioNumsRodado = Arrays.copyOfRange(nums, (tamanhoArray - k), (tamanhoArray));
+        int[] finalNumsRodado = Arrays.copyOfRange(nums, 0, tamanhoArray -k);
 
-        for (int i = 0; i < l; i++) {
-            for (int x = 0; x < nums.length; x++) {
+        rotated = Stream.concat(Arrays.stream(inicioNumsRodado).boxed(), Arrays.stream(finalNumsRodado).boxed())
+                .mapToInt(Integer::intValue)
+                .toArray();
 
-            }
+        for (int i = 0; i < tamanhoArray; i++) {
+            nums[i] = rotated[i];
         }
-
         return nums;
     }
 }
